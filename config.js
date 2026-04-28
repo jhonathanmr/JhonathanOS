229492702a918c3ceee728d664570dba5b2e08bc
jhonathanmr <jhonathanm2011@gmail.com>
2026-04-28 13:23:15 -0500

    Seguridad: Removiendo config.js del seguimiento de Git

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBYbdLe92IsQdgzLBwgOPhLc1q_UvuESxk",
-    authDomain: "nomina-mariachi.firebaseapp.com",
-    databaseURL: "https://nomina-mariachi-default-rtdb.firebaseio.com",
-    projectId: "nomina-mariachi",
-    storageBucket: "nomina-mariachi.firebasestorage.app",
-    messagingSenderId: "22825374302",
-    appId: "1:22825374302:web:a7918dcb3969d2705b1acf"
-};
