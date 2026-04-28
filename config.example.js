efc73974c9f3aebbced23a94f12a14c4061d07dc
jhonathanmr <jhonathanm2011@gmail.com>
2026-04-28 13:27:42 -0500

    Seguridad: Agregada plantilla de configuración y removidas credenciales reales

---
+++ config.example.js
@@ -0,0 +1,14 @@
+// JhonathanOS - Template de Configuración
+// INSTRUCCIONES:
+// 1. Renombra este archivo a 'config.js'
+// 2. Completa los datos con tus credenciales de Firebase Console
+
+export const firebaseConfig = {
+    apiKey: "TU_API_KEY_AQUI",
+    authDomain: "tu-proyecto.firebaseapp.com",
+    databaseURL: "https://tu-proyecto-default-rtdb.firebaseio.com",
+    projectId: "tu-proyecto-id",
+    storageBucket: "tu-proyecto.appspot.com",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID"
+};
