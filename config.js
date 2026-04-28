47efe9c35918b3275f122662389671ebd66e91ec
jhonathanmr <jhonathanm2011@gmail.com>
2026-04-28 19:41:36 +0000

    Deploying to gh-pages from @ jhonathanmr/JhonathanOS@4c813143f9c383ef5483e29ae927a30a7655ca3a 🚀

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+export const firebaseConfig = export const firebaseConfig = {
     apiKey: AIzaSyBYbdLe92IsQdgzLBwgOPhLc1q_UvuESxk,
     authDomain: nomina-mariachi.firebaseapp.com,
     databaseURL: https://nomina-mariachi-default-rtdb.firebaseio.com,
@@ -6,4 +6,4 @@ export const firebaseConfig = {
     storageBucket: nomina-mariachi.firebasestorage.app,
     messagingSenderId: 22825374302,
     appId: 1:22825374302:web:a7918dcb3969d2705b1acf
-};
+};;
